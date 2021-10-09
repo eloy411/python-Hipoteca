b5b40a1ab0bafc2ee3867b0dd480375bc4d66ab6
eloy411 <91909651+eloy411@users.noreply.github.com>
2021-10-09 19:09:25 +0200

    Update hipote.py

--- hipote.py
+++ hipote.py
@@ -8,3 +8,11 @@ if importeP <= costeP*0.8 and importeMR < ingresosA/12 * 0.5:
   
 else:print("hipoteca no concedida")
 # HACIENDO PRUEBAS
+
+# importeP = 80000
+# ingresosA = 35000
+# costeP = 100000
+# terminiP = 35 años
+# importeMR = 80000*2/(35*12)
+# if 80000 <= 100000 * 0.8 and importeMR < 35000/12 * 0.5:
+#         print hipotreca concedida
